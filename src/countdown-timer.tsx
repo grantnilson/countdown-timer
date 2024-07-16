16bc7614e4f14dcb8130b724c7934555e84f160f
Grant Nilson <gnilson314@gmail.com>
2024-07-16 13:20:32 -0700

    skel added

--- src/countdown-timer.tsx
+++ src/countdown-timer.tsx
@@ -1,12 +1,23 @@
 import { useState } from "react";
 
-const fiveMinutes = new Date(0, 0, 0, 0, 5);
+const formatDate = (dateObject) => {
+  const minutes = dateObject.getMinutes();
+  const seconds = dateObject.getSeconds();
+  const paddedSeconds = seconds.toString().padStart(2, "0");
+  return `${minutes}:${paddedSeconds}`;
+};
 
-const minutes = fiveMinutes.getMinutes();
-const seconds = fiveMinutes.getSeconds();
-
-console.log(`${minutes}:${seconds}`);
-
-const CountdownTimer = () => <div>Add your timer here!</div>;
-
-export default CountdownTimer;
+export default function CountdownTimer() {
+  const fiveMinutes = new Date(0, 0, 0, 0, 5);
+  const [timeRemaining, setTimeRemaining] = useState(fiveMinutes);
+  return (
+    <div>
+      <h1>{formatDate(timeRemaining)}</h1>
+      <div>
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  );
+}
