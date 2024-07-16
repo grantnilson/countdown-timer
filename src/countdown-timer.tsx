87ff9c3c35a75010fc1ee9767562bbe68333b885
Grant Nilson <gnilson314@gmail.com>
2024-07-16 12:56:33 -0700

    node module stuff

--- src/countdown-timer.tsx
+++ src/countdown-timer.tsx
@@ -1,3 +1,12 @@
+import { useState } from "react";
+
+const fiveMinutes = new Date(0, 0, 0, 0, 5);
+
+const minutes = fiveMinutes.getMinutes();
+const seconds = fiveMinutes.getSeconds();
+
+console.log(`${minutes}:${seconds}`);
+
 const CountdownTimer = () => <div>Add your timer here!</div>;
 
 export default CountdownTimer;
